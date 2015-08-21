7a84989a163e5fb0431f2e6e191083020fb65de9
Jason Brackins <jason.brackins@gmail.com>
2015-08-20 23:35:33 -0700

    Reference kind of works with a built in server
    But it doesn’t exit properly yet.

--- css-unit.js
+++ css-unit.js
@@ -1,13 +1,49 @@
 var fs = require('fs'),
     _ = require('lodash'),
+    mkdirp = require('mkdirp'),
+    finalhandler = require('finalhandler'),
+    http = require('http'),
+    serveStatic = require('serve-static'),
     driver = require('node-phantom-simple');
 
-var testTempalte = _.template('<html><head><% _.forEach(cssDependencies, function(dep) { %><link rel="stylesheet" href="<%= dep %>"><% }); %></head><body><%= testContents %></body></html>');
+var port = 9356,
+    testTempalte, server, options;
+
+
+function CSSUnit(opts) {
+  options = opts;
+  if( opts.templateFile ) {
+    var tplFile = fs.readFileSync(process.cwd() + '/' + opts.templateFile);
+    testTempalte = _.template(tplFile);
+  } else {
+    testTempalte = _.template('<html><head><% _.forEach(cssDependencies, function(dep) { %><link rel="stylesheet" href="<%= dep %>"><% }); %></head><body><%= testContents %></body></html>');
+  }
+  startServer();
+};
+
+
+function startServer() {
+  // Serve up public/ftp folder
+  var serve = serveStatic(process.cwd());
+
+  // Create server
+  server = http.createServer(function(req, res) {
+    var done = finalhandler(req, res);
+    serve(req, res, done);
+  })
+  console.log('serving ', process.cwd(), 'on port ', port);
+  server.listen(port);
+}
+
+function stopServer() {
+  server.close();
+}
 
 
 function renderTest(file, outputPath) {
+  console.log('render');
   var context = {
-      cssDependencies: [],
+      cssDependencies: (options.cssDependencies) ? options.cssDependencies : '',
       testContents: file.contents
     },
     rendered = testTempalte(context);
@@ -18,7 +54,8 @@ function renderTest(file, outputPath) {
 
 
 function runTest(url, outputPath) {
-  console.log('runtest', url, outputPath);
+  // startServer();
+  // console.log('runtest', url, outputPath);
   driver.create({ path: require('slimerjs').path }, function(err, browser) {
     return browser.createPage(function(err, page) {
       return page.open(url, function(err, status) {
@@ -26,32 +63,32 @@ function runTest(url, outputPath) {
         page.render(outputPath);
         browser.exit();
       });
-
     });
   });
+  // stopServer();
 }
 
+CSSUnit.prototype.reference = function refernce(file) {
+  // create dir
+  var destPath = process.cwd() + '/test/temp/' + file.relative.substr(0, file.relative.lastIndexOf('/'));
+  mkdirp.sync(destPath);
 
-
-
-module.exports.reference = function refernce(file) {
-
-  var htmlFilePath = '/temp/' + file.relative+'.html',
+  var htmlFilePath = '/test/temp/' + file.relative+'.html',
       compliedTest = renderTest(file, process.cwd() + htmlFilePath);
-  console.log('css-unit reference', compliedTest);
+  console.log('css-unit reference');
 
-  runTest('http://localhost:9356'+htmlFilePath, process.cwd() + '/reference/' + file.relative + '.png');
-
-
+  runTest('http://localhost:'+port+htmlFilePath, process.cwd() + '/test/reference/' + file.relative + '.png');
 }
 
 
-module.exports.test = function test(file) {
+CSSUnit.prototype.test = function test(file) {
   var compliedTest = renderTest(file);
   var compliedTest = renderTest(file);
-  runTest('http://localhost:8000/'+file.relative, process.cwd() + '/test');
+  runTest('http://localhost:'+port+htmlFilePath, process.cwd() + '/test/fresh/' + file.relative + '.png');
 
   // compare();
 
-  console.log('css-unit test', compliedTest);
 }
+
+
+module.exports = CSSUnit;
