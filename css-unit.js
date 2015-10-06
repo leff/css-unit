5ae7e9cbb6ea8ea9a8609d20e60839ca34d6acb6
Jason Brackins <jason.brackins@gmail.com>
2015-10-06 16:25:33 -0700

    Basic comparison is working
    Thanks to node-resemble-js.

--- css-unit.js
+++ css-unit.js
@@ -1,10 +1,12 @@
-var fs = require('fs'),
+var q = require('q'),
+    fs = require('fs'),
+    http = require('http'),
     _ = require('lodash'),
     mkdirp = require('mkdirp'),
     finalhandler = require('finalhandler'),
-    http = require('http'),
     serveStatic = require('serve-static'),
-    driver = require('node-phantom-simple');
+    driver = require('node-phantom-simple'),
+    resemble = require('node-resemble-js');
 
 var port = 9356,
     testTempalte, server, options;
@@ -18,7 +20,6 @@ function CSSUnit(opts) {
   } else {
     testTempalte = _.template('<html><head><% _.forEach(cssDependencies, function(dep) { %><link rel="stylesheet" href="<%= dep %>"><% }); %></head><body><%= testContents %></body></html>');
   }
-  startServer();
 };
 
 
@@ -31,7 +32,7 @@ function startServer() {
     var done = finalhandler(req, res);
     serve(req, res, done);
   })
-  console.log('serving ', process.cwd(), 'on port ', port);
+  // console.log('serving ', process.cwd(), 'on port ', port);
   server.listen(port);
 }
 
@@ -41,52 +42,82 @@ function stopServer() {
 
 
 function renderTest(file, outputPath) {
-  console.log('render');
   var context = {
       cssDependencies: (options.cssDependencies) ? options.cssDependencies : '',
       testContents: file.contents
     },
     rendered = testTempalte(context);
-
   fs.writeFileSync(outputPath, rendered)
   return rendered;
 }
 
 
 function runTest(url, outputPath) {
-  // startServer();
+  startServer();
   // console.log('runtest', url, outputPath);
+  var deferred = q.defer();
+  deferred.promise.then(stopServer);
+
   driver.create({ path: require('slimerjs').path }, function(err, browser) {
     return browser.createPage(function(err, page) {
       return page.open(url, function(err, status) {
-        console.log("opened site? ", status);
         page.render(outputPath);
         browser.exit();
+        deferred.resolve();
       });
     });
   });
-  // stopServer();
+  return deferred.promise;
 }
 
+function compare(refImg, newImg, diffImgPath) {
+  var deferred = q.defer();
+
+  var diff = resemble(refImg).compareTo(newImg).onComplete(function(data) {
+    if( Number(data.misMatchPercentage) >= 0.01 ) {
+      data.getDiffImage().pack().pipe(fs.createWriteStream(process.cwd() + diffImgPath));
+      deferred.reject(new Error('Unacceptable Difference (' + data.misMatchPercentage + ')\n   Reference: ' + refImg + '\n   New: ' + newImg + '\n   Diff: ' + process.cwd() + diffImgPath ));
+    } else {
+      deferred.resolve();
+    }
+  });
+  return deferred.promise;
+}
+
+
 CSSUnit.prototype.reference = function refernce(file) {
-  // create dir
-  var destPath = process.cwd() + '/test/temp/' + file.relative.substr(0, file.relative.lastIndexOf('/'));
+  var destPath = process.cwd() + '/temp/reference/' + file.relative.substr(0, file.relative.lastIndexOf('/'));
   mkdirp.sync(destPath);
 
-  var htmlFilePath = '/test/temp/' + file.relative+'.html',
-      compliedTest = renderTest(file, process.cwd() + htmlFilePath);
-  console.log('css-unit reference');
+  var htmlFilePath = '/temp/reference/' + file.relative,
+      htmlFileUrl = 'http://localhost:'+port+htmlFilePath,
+      renderedPngPath = process.cwd() + '/test/reference/' + file.relative + '.png';
 
-  runTest('http://localhost:'+port+htmlFilePath, process.cwd() + '/test/reference/' + file.relative + '.png');
+  renderTest(file, process.cwd() + htmlFilePath);
+
+  console.log('creating ref: ' + renderedPngPath);
+  return runTest(htmlFileUrl, renderedPngPath);
 }
 
 
 CSSUnit.prototype.test = function test(file) {
-  var compliedTest = renderTest(file);
-  var compliedTest = renderTest(file);
-  runTest('http://localhost:'+port+htmlFilePath, process.cwd() + '/test/fresh/' + file.relative + '.png');
+  var dirPath = process.cwd() + '/temp/compare/' + file.relative.substr(0, file.relative.lastIndexOf('/'));
+  mkdirp.sync(dirPath);
+  dirPath = process.cwd() + '/test/diff/' + file.relative.substr(0, file.relative.lastIndexOf('/'));
+  mkdirp.sync(dirPath);
 
-  // compare();
+  var htmlFilePath = '/temp/compare/' + file.relative,
+      htmlFileUrl = 'http://localhost:'+port+htmlFilePath,
+      refImgPath = process.cwd() + '/test/reference/' + file.relative + '.png',
+      newImgPath = process.cwd() + '/test/compare/'   + file.relative + '.png';
+
+  renderTest(file, process.cwd() + htmlFilePath);
+
+  return runTest(htmlFileUrl, newImgPath)
+  .then(function() {
+    console.log('comparing => ' + file.relative);
+    return compare(refImgPath, newImgPath, '/test/diff/' + file.relative + '.png');
+  });
 
 }
 
